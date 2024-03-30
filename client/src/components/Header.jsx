d7d26fb061ff83ed275f69dca67b3e6339b089ff
Navin Kumaran <navinkumaran2004@gmail.com>
2024-03-30 16:07:56 +0530

    button outline in header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -30,14 +30,14 @@ const Header = () => {
           <FaMoon />
         </Button>
         <Link to="/sign-in" >
-          <Button gradientDuoTone="purpleToBlue">
+          <Button gradientDuoTone="purpleToBlue" outline>
             Sign In
           </Button>
         </Link>
         <NavbarToggle />
       </div>
       <Navbar.Collapse>
-        <Navbar.Link active={path === '/'} as={'div'}> 
+        <Navbar.Link active={path === '/'} as={'div'}>
           <Link to='/'>Home</Link>
         </Navbar.Link>
         <Navbar.Link active={path === "/about"} as={'div'}>
