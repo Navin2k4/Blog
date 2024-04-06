ec251ef874c9d5d2e7dc3151dfa4874bb186cf42
Navin Kumaran <navinkumaran2004@gmail.com>
2024-04-06 20:01:09 +0530

    Complete Profile Page UI

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,10 +1,27 @@
-
+import { Button, TextInput } from "flowbite-react";
+import { useSelector } from "react-redux"
 function DashProfile() {
-  return (
-    <div>
-      DashProfile
-    </div>
-  )
+    const { currentUser } = useSelector((state) => state.user);
+    return (
+        <div className="max-w-lg mx-auto p-3 w-full">
+            <h1 className="my-7 text-center font-semibold text-3xl">Profile</h1>
+            <form className="flex flex-col gap-4">
+                <div className="w-32 h-32 mb-4 self-center cursor-pointer shadow-lg overflow-hidden rounded-full">
+                    <img src={currentUser.profilePicture} alt="UserProfile" className="rounded-full w-full h-full border-4 object-cover border-blue-500  " />
+                </div>
+                <TextInput type='text' id="username" placeholder="Username" defaultValue={currentUser.username} />
+                <TextInput type='email' id="email" placeholder="Email" defaultValue={currentUser.email} />
+                <TextInput type='password' id="password" placeholder="password" />
+                <Button type="submit" className="bg-gradient-to-r from-red-600 to-blue-600 " outline>
+                    Update
+                </Button>
+                <div className="text-red-500 flex justify-between mt-5">
+                    <span className="cursor-pointer">Delete account</span>
+                    <span className="cursor-pointer">Sign Out</span>
+                </div>
+            </form>
+        </div>
+    )
 }
 
 export default DashProfile
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -28,16 +28,19 @@ const Header = () => {
           className="hidden lg:inline"
         />
       </form>
-      <Button className="w-12 h-10 pt-1 lg:hidden" color="gray" pill>
+      <Button className="w-12 h-10 lg:hidden" color="gray" pill>
         <AiOutlineSearch />
       </Button>
       <div className="flex gap-2
       md:order-2">
 
-        <Button className="w-12 h-10 hidden sm:inline" color="gray" pill onClick={() => dispatch(toggleTheme())}>
-
+        <Button
+          className='w-12 h-10'
+          color='gray'
+          pill
+          onClick={() => dispatch(toggleTheme())}
+        >
           {theme === 'light' ? <FaSun /> : <FaMoon />}
-          
         </Button>
 
         {currentUser ? (
