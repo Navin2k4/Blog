9d51b84b3cb3a2049c0f320956ab64805f27ccff
Navin Kumaran <navinkumaran2004@gmail.com>
2024-04-08 22:35:14 +0530

    Show user post in dashboard

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,17 +1,17 @@
-/** @type {import('tailwindcss').Config} */
-import flowbitePlugin from "flowbite/plugin";
+import flowbitePlugin from 'flowbite/plugin';
+import tailwindScrollbar from 'tailwind-scrollbar';
 
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "node_modules/flowbite-react/lib/esm/**/*.js",
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
     extend: {},
   },
   plugins: [
-    // ...
     flowbitePlugin,
+    tailwindScrollbar,
   ],
 };
